3cd45adc63316677e440cd823708b2f20e58c063
gaohuaid <gaohuacode@gmail.com>
2019-06-02 04:43:39 +0800

    Exercise 2.4

---
+++ ch2/ex2_4/main.go
@@ -0,0 +1,27 @@
+/*
+ * Exercise 2.4: Write a version of PopCount that counts bits by shifting its argument through 64
+ * bit positions, testing the rightmost bit each time.
+ * Author: gaohuaid
+ * Date: 2019/06/02
+ */
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	fmt.Println(PopCount(8))
+}
+
+func PopCount(x uint64) int{
+	var mask uint64 = 1
+	var count int = 0
+	for i := 0; i < 64; i++{
+		if (x & mask) == 1{
+			count ++
+		}
+		x = x >> 1
+	}
+	return count
+}
